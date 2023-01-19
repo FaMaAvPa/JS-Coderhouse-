6365f25cf2daaa7e6a91bac5f1fa1f430ca47a8c
Facundo Avalía <facuavalia011@gmail.com>
2023-01-19 18:48:49 -0300

    Segunda pre entrega

--- index.js
+++ index.js
@@ -47,3 +47,50 @@ let esMayor = registrarte()
 if (esMayor) { alert("Bienvenido " + nombre + " tenemos las mejores ofertas para ti!")}
 
 else { alert("Bienvenido " + nombre + " a la hora de hacer una compra requeriras permiso de un mayor.") }
+
+
+
+//ahora habrá una función que permita al usuario comprar productos
+//primero crearé un constructor para los productos (que serían objetos)
+
+
+function Producto(nombre, precio) {
+    this.nombre = nombre;;
+    this.precio = precio;
+}
+
+//array donde se almacnenarán los productos
+let productos = []
+
+//función que carga una lista de productos a comprar
+function cargarLista(cantProductos)
+{
+    
+   
+    for (let index = 1; index <= cantProductos; index++) {
+        let nombre = prompt("¿Cual es el producto " + index + " que desea comprar?");
+        let precio = Number(prompt("¿A que precio se encuentra?"));
+        productos.push(new Producto(nombre, precio));   
+    }
+
+}
+
+//función que mostrará el precio final de los productos
+function mostrarFinal(lista, cantProductos) 
+{
+    let precioFinal = 0
+    let precio = 0
+    for (let index = 0; index < productos.length; index++) 
+    {
+        Number(precio = productos[index].precio);
+        Number(precioFinal = precioFinal + precio);
+    }
+    alert("Gracias por comprar con nosotros, el precio de su compra será de: $" + precioFinal);
+}
+
+//pregunta cuantos prodcutos quiere cargar
+i = prompt("¿Cuantos productos quiere cargar?");
+//ejecuta la función de cargar lista
+cargarLista(i);
+//ejecuta la función que mostrará el precio final de la compra
+mostrarFinal(productos,i);
